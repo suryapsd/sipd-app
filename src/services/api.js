07af9cd757570049a279e34afb207490691368ed
suryapsd <desuryaa1@gmail.com>
2025-10-29 16:01:42 +0800

    first-commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
+// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
+  baseURL:
+    "https://script.google.com/macros/s/AKfycbywmzh4FYUaNRnv3yNlbS-k_1e-ra5r4EN19q9tMITG3aPTXA1OyQTcaAsuu4EixARn/exec",
   headers: {
     "Content-Type": "application/json",
   },
